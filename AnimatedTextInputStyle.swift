1ecd0eccec910c59ef8a0c751881ae20b8234ccd
Vladislav <sedinkin1993@gmail.com>
2016-10-28 10:44:54 +0600

    first commit

--- AnimatedTextInputStyle.swift
+++ AnimatedTextInputStyle.swift
@@ -19,20 +19,20 @@ public protocol AnimatedTextInputStyle {
 
 public struct AnimatedTextInputStyleBlue: AnimatedTextInputStyle {
 
-    public let activeColor = UIColor(red: 51.0/255.0, green: 175.0/255.0, blue: 236.0/255.0, alpha: 1.0)
+    public let activeColor = UIColor(hexString:"0076FF")
     public let inactiveColor = UIColor.grayColor().colorWithAlphaComponent(0.5)
-    public let lineInactiveColor = UIColor.grayColor().colorWithAlphaComponent(0.2)
-    public let errorColor = UIColor.redColor()
-    public let textInputFont = UIFont.systemFontOfSize(14)
+    public let lineInactiveColor = UIColor(hexString:"0076FF")
+    public let errorColor = UIColor(hexString:"EC0D00")
+    public let textInputFont = UIFont.systemFontOfSize(16)
     public let textInputFontColor = UIColor.blackColor()
     public let placeholderMinFontSize: CGFloat = 9
     public let counterLabelFont: UIFont? = UIFont.systemFontOfSize(9)
-    public let leftMargin: CGFloat = 25
-    public let topMargin: CGFloat = 20
+    public let leftMargin: CGFloat = 0
+    public let topMargin: CGFloat = 10
     public let rightMargin: CGFloat = 0
-    public let bottomMargin: CGFloat = 10
-    public let yHintPositionOffset: CGFloat = 7
-    public let yPlaceholderPositionOffset: CGFloat = 0
+    public let bottomMargin: CGFloat = 0
+    public let yHintPositionOffset: CGFloat = -2
+    public let yPlaceholderPositionOffset: CGFloat = 5
 
     public init() { }
 }
